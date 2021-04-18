68c13c8d4081e313b18238316a9caa8633492724
Vihaan15 <73476143+Vihaan15@users.noreply.github.com>
2021-04-18 11:59:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ function take_snapshot()
 console.log('ml5 version:' , ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/sua7gRke1/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded|')
+}
+
+function check() {
+    img =document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_person_name").innerHTML = results[0].label;
+        document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
